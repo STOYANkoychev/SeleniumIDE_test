6039c081e542892b4d0640550a9b93c37d91f600
STOYANkoychev <music.designed.6@gmail.com>
2024-08-01 09:47:31 +0300

    Fix type error in YAML fail

--- SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
+++ SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
@@ -22,7 +22,7 @@ public class TC01IfUserIsInvalidTryAgainTest
 
 
 
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
         js = (IJavaScriptExecutor)driver;
         vars = new Dictionary<string, object>();
     }
